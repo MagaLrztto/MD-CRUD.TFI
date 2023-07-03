020d2a4767789399f9185c72a2675f21b68033ad
MaGa <maga.lrztto@gmail.com>
2023-07-03 16:11:54 -0300

    modified:   js/producto_editar.js 	modified:   js/producto_nuevo.js 	modified:   producto_editar.html 	modified:   producto_nuevo.html 	modified:   productos.html

--- js/producto_editar.js
+++ js/producto_editar.js
@@ -1,61 +1,51 @@
-console.log(location.search) // lee los argumentos pasados a este formulario
-var id=location.search.substr(4)
-console.log(id)
-const { createApp } = Vue
+console.log(location.search)
 
-    createApp({
-        data() {
-            return {
-                id:0,
-                nombre:"",
-                imagen:"",
-                stock:0,
-                precio:0,
-                url:'https://magalrztto.pythonanywhere.com/productos/'+id,
-            }
-        },
-    methods: {
-        fetchData(url) {
-            fetch(url)
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data)
-                    this.id = data.id
-                    this.nombre = data.nombre;
-                    this.imagen = data.imagen
-                    this.stock = data.stock
-                    this.precio = data.precio
-                })
-                .catch(err => {
-                    console.error(err);
-                    this.error=true
-                })
-        },
-        modificar() {
-            let producto = {
-                nombre:this.nombre,
-                precio: this.precio,
-                stock: this.stock,
-                imagen:this.imagen
-            }
-            var options = {
-                body: JSON.stringify(producto),
-                method: 'PUT',
-                headers: { 'Content-Type': 'application/json' },
-                redirect: 'follow'
-            }
-            fetch(this.url, options)
-                .then(function () {
-                    alert("Registro modificado")
-                    window.location.href = "./productos.html";
-                })
-                .catch(err => {
-                    console.error(err);
-                    alert("Error al Modificar")
-                })
-            }
-        },
-        created() {
-            this.fetchData(this.url)
-        },
-    }).mount('#app')
+let argsUrl = location.search.substring(1).split('&');
+    console.log(argsUrl)
+
+let data = [];
+for(let i = 0; i< argsUrl.length; i++) {
+    data[i] = argsUrl[i].split('=');
+}
+
+console.log(data)
+
+document.getElementById('id').value = decodeURIComponent(data[0][1]);
+document.getElementById('nombre').value = decodeURIComponent(data[1][1]);
+document.getElementById('precio').value = decodeURIComponent(data[2][1]);
+document.getElementById('stock').value = decodeURIComponent(data[3][1]);
+document.getElementById('imagen').value = decodeURIComponent(data[4][1]);
+
+function modificar(){
+    let id = document.getElementById('id').value;
+    let n = document.getElementById('nombre').value;
+    let p = document.getElementById('precio').value;
+    let s = document.getElementById('stock').value;
+    let i = document.getElementById('imagen').value;
+    
+    let producto = {
+        nombre: n,
+        precio: p,
+        stock: s,
+        imagen: i
+    }
+
+    let url = 'https://magalrztto.pythonanywhere.com/productos/' + id;
+    
+    let options = {
+        body: JSON.stringify(producto),
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+    }
+        
+    fetch(url, options)
+        .then(function () {
+            alert("Registro modificado exitosamente")
+            window.location.href = "./productos.html";
+    })
+    
+    .catch(err => {
+        alert("No pudo modificarse el registro")
+        console.error(err);
+    })
+}
--- js/producto_nuevo.js
+++ js/producto_nuevo.js
@@ -1,53 +1,32 @@
-const {createApp} = Vue
+function guardar(){
+    let n = document.getElementById('nombre').value;
+    let p = document.getElementById('precio').value;
+    let s = document.getElementById('stock').value;
+    let i = document.getElementById('imagen').value;
 
-createApp({
-    data(){
-        return{
-            productos:[],
-            url:'https://magalrztto.pythonanywhere.com/productos',
-            cargando:true,
-            error: false
-        }    
-    },
+    let producto = {
+        nombre: n,
+        precio: p,
+        stock: s,
+        imagen: i
+    }
 
-    methods:{
-        fetchData(url){
-            fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                this.productos = data;
-                this.cargando = false;
-            })
-            .catch(err => {
-                console.error(err); //console.error = console.log pero en color rojo
-                this.error = true;
-            })
-        },
-        grabar(){
-        let producto = {
-            nombre:this.nombre,
-            precio: this.precio,
-            stock: this.stock,
-            imagen:this.imagen
-        }
-        var options = {
-            body:JSON.stringify(producto),
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            redirect: 'follow'
-        }
-        fetch(this.url, options)
-            .then(function () {
-                alert("Registro grabado")
-                window.location.href = "./productos.html";
-            })
-            .catch(err => {
-                console.error(err);
-                alert("Error al Grabar")
-            })
-        }
-    },        
-    created(){
-        this.fetchData(this.url);
-    },
-}).mount('#app')
+    let url = 'https://magalrztto.pythonanywhere.com/productos';
+    
+    let options = {
+        body: JSON.stringify(producto),
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+    }
+        
+    fetch(url, options)
+        .then(function () {
+            alert("Registro guardado exitosamente")
+            window.location.href = "./productos.html";
+    })
+    
+    .catch(err => {
+        alert("No pudo guardarse el nuevo producto")
+        console.error(err);
+    })
+}
